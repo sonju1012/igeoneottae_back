7e2e60f1d24a466a13b6601a9a76ba078a19bb87
moon gyo <smk354@naver.com>
2020-11-02 13:59:31 +0900

    오늘커밋해봄

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
-var router = express.Router();
+var router = express();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+  res.send('index');
 });
+
+var port = 2000;
+router.listen(port, function(){
+  console.log('server on! https://localhost:'+port);
+})
 
 module.exports = router;
--- routes/user/users.js
+++ routes/user/users.js
@@ -1,32 +1,32 @@
-var express = require('express');
-var router = express.Router();
-var db = require('../../db/db_connection')
+// var express = require('express');
+// var router = express.Router();
+// var db = require('../../db/db_connection')
 
-/* GET users listing. */
-var dd =  new db();
-console.log(dd(aa => {
-  aa.get('users', (err, response) => {
-      if(err) return err;
-      return response;
-    })
-}))
+// /* GET users listing. */
+// var dd =  new db();
+// console.log(dd(aa => {
+//   aa.get('users', (err, response) => {
+//       if(err) return err;
+//       return response;
+//     })
+// }))
 
-router.get('/', function(req, res, next) {
-  // db()(aa => {
-  //   aa.get('users', (err, response) => {
-  //       if(err) return err;
-        res.send('123');
-  //     })
-  //   })
-  // })
-    // db(aa=> {
-    //   console.log(aa);
-    //   aa.get('users', (err, response) => {
-    //       if(err) return err;
-    //       else res.send(response);
-    //     })
-    //   }
-    // );
-});
+// router.get('/', function(req, res, next) {
+//   // db()(aa => {
+//   //   aa.get('users', (err, response) => {
+//   //       if(err) return err;
+//         res.send('123');
+//   //     })
+//   //   })
+//   // })
+//     // db(aa=> {
+//     //   console.log(aa);
+//     //   aa.get('users', (err, response) => {
+//     //       if(err) return err;
+//     //       else res.send(response);
+//     //     })
+//     //   }
+//     // );
+// });
 
-module.exports = router;
+// module.exports = router;
